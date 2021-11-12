8113bd452e595607303b4bf9d2c18c6db82e93b4
LC <laleh.cabassu@gmail.com>
2021-11-12 09:28:23 -0500

    switch to react-player, improve styles, and update gitignore

--- src/components/VideoPlayer/VideoPlayer.model.ts
+++ src/components/VideoPlayer/VideoPlayer.model.ts
@@ -2,3 +2,7 @@ export interface VideoPlayerState {
     playerHeight: number;
     playerWidth: number;
 }
+
+export interface VideoPlayerProps {
+    uri: string;
+}
--- src/components/VideoPlayer/VideoPlayer.test.tsx
+++ src/components/VideoPlayer/VideoPlayer.test.tsx
@@ -3,9 +3,11 @@ import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import VideoPlayer from './VideoPlayer';
 
+const uri = "http://data.phys.ucalgary.ca/sort_by_project/AuroraMAX/rt-movies/mp4/2021/11/04/auroramaxHD_20211104_720p.mp4";
+
 describe('<TestComponent />', () => {
     test('it should mount', () => {
-        render(<VideoPlayer/>);
+        render(<VideoPlayer uri={uri} />);
 
         const testComponent = screen.getByTestId('TestComponent');
 
